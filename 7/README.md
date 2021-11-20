f689167b7573b35eee2bb1e537a50fed0b0f773f
rodolfo0259 <contieri.rodolfo@gmail.com>
2021-11-20 17:53:17 -0300

    Organizacao e Documentacao

---
+++ 7/README.md
@@ -0,0 +1,26 @@
+# Coletas os dados de todos os videos de um canal do Youtube utilizando a Youtube_API
+## Get data of every video from a Youtube channel using the Youtube_API
+
+<br>
+
+Os dados coletados são:
+
+> Id do Video, Titulo, Data de Upload, Duracao, Views, Likes, Dislikes, Favoritos, quantidade de comentarios
+
+Tambem há um campo calculado: Total de Views/dias desde lancamento do video, é uma métrica para reconhecer Videos que obtiveram maior audiencia no menor tempo ( média de views desde lançamento do video )
+
+
+## **Passos de instalação/uso**
+
+1. Clonar repositório
+2. Adicionar os IDs dos canais do Youtube no settings.ini
+3. Adicionar sua Google API key no setting.ini
+4. Instalar as dependencias
+5. Executar
+
+```
+git clone https://github.com/rodolfo0259/NappAcademy2-2021.git
+pip install -r requirements.txt 
+```
+
+No settings.ini o campo CHANNEL_ID, pode conter mais de um id de canal, separado por ","
--- 7/youtube_stats.py
+++ 7/youtube_stats.py
@@ -3,24 +3,23 @@ Coleta os detalhes de todos os videos de um determinado canal no youtube
 e transforma os dados em um CSV
 Dados coletados: 
 Id_video, Titulo, DataUpload, duracao, views, likes, dislikes, favoritos, qtd_comentarios
-"""
-import asyncio
-import aiohttp
-import requests
-import time
-import pandas as pd
-from datetime import datetime
-from datetime import timedelta
-import re
-
-from API_KEY import KEY
 
 ## API DOC
 ## https://developers.google.com/youtube/v3/docs/videos/list
+"""
+import asyncio
+import time
+from datetime import datetime
+from datetime import timedelta
+import requests
+import pandas as pd
+import aiohttp
+import re
 
+from decouple import config, Csv
 
-CHANNEL_ID = 'UCK3IOOEUXQT-dAl3jy-xkaw' ## BAKA GAIJIN # 24 vids
-# CHANNEL_ID = 'UC5e2_0A3A0AvMaZUecvXqZQ'  ## gardarkeres # 5 vids
+KEY = config('KEY')
+CHANNEL_IDS = config('CHANNEL_ID', cast=Csv())
 
 
 def get_video_ids(channel_id: str)->list:
@@ -68,56 +67,79 @@ async def main(video_ids: list)->None:
             task = asyncio.ensure_future(get_video_data(session, video_id))
             tasks.append(task)
 
-        video_statistics = await asyncio.gather(*tasks)
+        videos_data = await asyncio.gather(*tasks)
+        video_data_list = list()
+        for video_data in videos_data:
+            video_data_list.append(parse_video_data(video_data))
+        
+        generate_csv(video_data_list)
 
-    headers=['Video_id', 'Title', 'Publish_date', 'Duration', 'Views', 'Likes', 'Dislikes', 'FavoriteCount', 'CommentCount']
-    df = pd.DataFrame(video_statistics, columns=headers)
-    df.to_csv(f'channel_{channel_title}_statistics.csv', sep=';', index=False)
-    print('>> CSV gerado <<')
 
-
-async def get_video_data(session, video_id: str)->list:
+async def get_video_data(session, video_id: str)->dict:
     '''
     Coleta os dados de cada video_id
+    Args:
+        session: async session
+    Return:
+        video_data: dados coletados do video_id fornecido em json
     '''
     url = f'https://www.googleapis.com/youtube/v3/videos?id={video_id}&key={KEY}&part=statistics,contentDetails,snippet'
 
     async with session.get(url) as response:
-        lis = list()
         result_data = await response.json()
-        results = result_data['items']
-        global channel_title
-        channel_title = results[0]['snippet']['channelTitle']
-
-        ## UTF date to UTF-3 (local)
-        video_date = results[0]['snippet']['publishedAt']
-        video_date = datetime.strptime(video_date, '%Y-%m-%dT%H:%M:%SZ') - timedelta(hours=3)
-        video_date = video_date.strftime('%Y-%m-%d %H:%M:%S')
-        lis.append(results[0]['id'])
-        lis.append(results[0]['snippet']['title'])
-        lis.append(video_date)
-
-        ## format time duration to iso 8601
-        duration = (results[0]['contentDetails']['duration'])
-        final_duration = ''
-        for i in ['H', 'M', 'S']:
-            if duration.find(i) != -1:
-                final_duration += ((re.findall(f'(\d+){i}', duration))[0]).zfill(2) + ':'
-            else:
-                final_duration += '00:'
-
-        lis.append(final_duration[:-1]) ## video_duracao
-
-        ### views, like, dislike, favorite, comments
-        data = results[0]['statistics']
-        for k, v in data.items():
-            lis.append(v)
-
-        return lis
+        video_data = result_data['items'][0]
+        return video_data
 
 
-video_ids = get_video_ids(CHANNEL_ID)
-asyncio.run(main(video_ids))
+def parse_video_data(video_data:dict)->list:
+    '''
+    Tratamento dos dados coletados em json do video
+    Args:
+        video_data: json/dict com os dados do video
+    Return:
+        parsed_list: lista com os dados tratados
+    '''
+    parsed_list = list()
+    global channel_title
+    channel_title = video_data['snippet']['channelTitle']
+    parsed_list.append(video_data['id'])
+    parsed_list.append(video_data['snippet']['title'])
+
+    ## UTF date to UTF-3 (local)
+    video_date = video_data['snippet']['publishedAt']
+    video_date = datetime.strptime(video_date, '%Y-%m-%dT%H:%M:%SZ') - timedelta(hours=3)
+    parsed_list.append(video_date.strftime('%Y-%m-%d %H:%M:%S'))
+
+    ## format e.g. PT1H23M03S time duration to iso 8601
+    duration = (video_data['contentDetails']['duration'])
+    final_duration = ''
+    for i in ['H', 'M', 'S']:
+        if duration.find(i) != -1:
+            final_duration += ((re.findall(f'(\d+){i}', duration))[0]).zfill(2) + ':'
+        else:
+            final_duration += '00:'
+
+    parsed_list.append(final_duration[:-1]) ## video_duration
+
+    ### views, like, dislike, favorite, comments
+    data = video_data['statistics']
+    for k, v in data.items():
+        parsed_list.append(v)
+
+    ## Views/days since video release
+    view_day = int(data['viewCount'])/int((datetime.today() - video_date ).days)
+    parsed_list.append(round(view_day, 3))
+
+    return parsed_list
 
 
+def generate_csv(video_data_list:list)->None:
+    headers=['Video_id', 'Title', 'Publish_date', 'Duration', 'Views', 'Likes', 'Dislikes', 'FavoriteCount', 'CommentCount', 'View/day']
+    df = pd.DataFrame(video_data_list, columns=headers)
+    df.to_csv(f'channel_{channel_title}_statistics.csv', sep=';', index=False)
+    print(f'>> CSV gerado do canal {channel_title} <<')
 
+
+for CHANNEL_ID in CHANNEL_IDS:
+    video_ids = get_video_ids(CHANNEL_ID)
+    asyncio.run(main(video_ids))
